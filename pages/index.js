f546a50083ca88204ed29ced24be2500197803be
dev-ahmedali <ahmedali69325@gmail.com>
2023-06-15 14:57:42 +0600

    initial project

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+
+
+export default function Home() {
+  return (
+      <div className="bg-blue-900 w-screen h-screen flex items-center">
+        <div className="text-center w-full">
+          <button className="bg-white p-2 px-4 text-black rounded-lg">
+            Login with Google
+          </button>
+        </div>
+      </div>
+  )
+}
