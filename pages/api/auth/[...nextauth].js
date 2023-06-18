a28294541de02f4d6f8c043a960b35c4dddd7f88
dev-ahmedali <ahmedali69325@gmail.com>
2023-06-18 14:47:12 +0600

    connected mongodb

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,7 +1,7 @@
 import clientPromise from "@/lib/mongodb";
 import { MongoDBAdapter } from "@auth/mongodb-adapter";
 import NextAuth from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
+import GoogleProvider from "next-auth/providers/google";  
 
 export default NextAuth({
   providers: [
