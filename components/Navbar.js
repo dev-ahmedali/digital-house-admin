71ebd6fbd8242198a71925ed7478543c5d9d10c2
dev-ahmedali <ahmedali69325@gmail.com>
2023-06-19 16:21:45 +0600

    updated product mangement2

--- components/Navbar.js
+++ components/Navbar.js
@@ -44,22 +44,6 @@ export default function Navbar() {
           </svg>
           Dashboard
         </Link>
-        <Link href={"/orders"} className={pathname.includes("/orders") ? activeLink : inActiveLink}>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="currentColor"
-            class="w-6 h-6">
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M20.25 7.5l-.625 10.632a2.25 2.25 0 01-2.247 2.118H6.622a2.25 2.25 0 01-2.247-2.118L3.75 7.5M10 11.25h4M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125z"
-            />
-          </svg>
-          Orders
-        </Link>
         <Link href={"/products"} className={pathname.includes("/products") ? activeLink : inActiveLink}>
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -76,6 +60,23 @@ export default function Navbar() {
           </svg>
           Products
         </Link>
+        <Link href={"/orders"} className={pathname.includes("/orders") ? activeLink : inActiveLink}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="w-6 h-6">
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M20.25 7.5l-.625 10.632a2.25 2.25 0 01-2.247 2.118H6.622a2.25 2.25 0 01-2.247-2.118L3.75 7.5M10 11.25h4M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125z"
+            />
+          </svg>
+          Orders
+        </Link>
+        
         <Link href={"/settings"} className={pathname.includes("/settings") ? activeLink : inActiveLink}>
           <svg
             xmlns="http://www.w3.org/2000/svg"
--- pages/products/new.js
+++ pages/products/new.js
@@ -1,8 +1,20 @@
 import Layout from "@/components/Layout";
-
+import { useState } from "react";
 
 export default function NewProduct() {
+    const [title, setTitle] = useState('')
+    const [description, setDescription] = useState('')
+    const [price, setPrice] = useState('')
   return (
-    <Layout>New Product</Layout>
-  )
+    <Layout>
+      <h1>New product</h1>
+      <label>Product name</label>
+      <input type="text" placeholder="Product name" />
+      <label>Description</label>
+      <textarea placeholder="description"></textarea>
+      <label>Price (in USD)</label>
+      <input type="text" placeholder="price" />
+      <button className="btn-primary">Save</button>
+    </Layout>
+  );
 }
