dfe50089ddb9cf60d190de6fb8183578562d8cd5
dev-ahmedali <ahmedali69325@gmail.com>
2023-06-20 01:24:18 +0600

    updated product manaement4

--- pages/api/products.js
+++ pages/api/products.js
@@ -1,7 +1,21 @@
-export default function handle(req, res) {
+import { Product } from "@/lib/models/product";
+import clientPromise from "@/lib/mongodb";
+import mongoose from "mongoose";
+
+export default function async handle(req, res) {
     const {method} = req;
+    mongoose.Promise = clientPromise
+
 
     if(method === "POST") {
-        //
+        const  {title, description, post} = req.body
+        const before 
+        Product.create({
+           const ProductDoc = await product.create({
+            title, description, price
+           })  
+        })
+        res.json('post')
     }
 }
+
