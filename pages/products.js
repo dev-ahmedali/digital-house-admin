3261486d3614650767cd2cba7b9b782008e2a6b5
dev-ahmedali <ahmedali69325@gmail.com>
2023-06-21 14:08:00 +0600

    updated product m12

--- pages/products.js
+++ pages/products.js
@@ -30,7 +30,7 @@ export default function Products() {
               <tr key={product.id}>
                 <td>{product.title}</td>
                 <td>
-                  <Link href={"/products._id" + product._id}>
+                  <Link href={"/products/edit/" + product._id}>
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
                       fill="none"
