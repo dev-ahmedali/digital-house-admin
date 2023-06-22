ad7507d8259b4f27f3627b9b71322e9509556fd2
dev-ahmedali <ahmedali69325@gmail.com>
2023-06-23 02:14:41 +0600

    updated pm22

--- pages/api/upload.js
+++ pages/api/upload.js
@@ -1,3 +1,13 @@
+import multiparty from "multiparty"
+
 export default async function handle(req, res) {
-    
+    const form = new multiparty.Form();
+    form.parse(req, (err, fields, files) => {
+        console.log('length:', files?.length);
+        res.json('Ok')
+    })
+}
+
+export const config = {
+    api: {bodyParser: false}
 }
