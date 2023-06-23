a704da6d062bbdde93f290a2ffa610062e1de2f4
dev-ahmedali <ahmedali69325@gmail.com>
2023-06-23 14:44:43 +0600

    updated pm23

--- pages/api/upload.js
+++ pages/api/upload.js
@@ -2,14 +2,16 @@ import multiparty from "multiparty";
 
 export default async function handle(req, res) {
   const form = new multiparty.Form();
-  const { fields, files } = new Promise((resolve, reject) => {
+  const { fields, files } = await new Promise((resolve, reject) => {
     form.parse(req, (err, fields, files) => {
-      if (err) reject(err);
-      resolve({ fields, files });
+     if(err) {
+      reject(err)
+     }
+     resolve({fields, files})
     });
   });
-  console.log("length:", files?.length);
-  console.log(fields);
+  console.log("length:", files.file.length);
+  
   return res.json("Ok");
 }
 
